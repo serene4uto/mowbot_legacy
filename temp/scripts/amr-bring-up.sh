075809691a3db694db4ec57762c93fc9b5a29fa9
Zeki411 <nguyenhatrung411@gmail.com>
2025-04-08 16:50:30 +0900

    Refactor launch commands and update udev rules for improved device management

--- temp/scripts/amr-bring-up.sh
+++ temp/scripts/amr-bring-up.sh
@@ -4,8 +4,9 @@ docker start mowbot_legacy
 docker exec -it -e DISPLAY=:0.0 -w /workspaces/mowbot_legacy mowbot_legacy /bin/bash -c "\
         cd /workspaces/mowbot_legacy \
         && . ./install/setup.bash \
-        && ros2 launch mowbot_legacy_launch bringup.launch.py uros:=true foxglove:=true \
+        && ros2 launch mowbot_legacy_launch bringup.launch.py \
+            uros:=true foxglove:=true \
             imu:=true madgwick:=true \
-            ntrip:=true gpsl:=true gpsr:=true dgps_compass:=true \
+            ntrip:=true gps:=true \
             laser:=true \
     "
--- temp/scripts/term-amr-health-monitor.sh
+++ temp/scripts/term-amr-health-monitor.sh
@@ -4,7 +4,7 @@
 CONTAINER_NAME="mowbot_legacy"
 
 # Get the PID of the ROS2 launch command inside the Docker container
-ROS2_LAUNCH_PID=$(docker exec "$CONTAINER_NAME" pgrep -f "ros2 run py_mowbot_utils system_monitor")
+ROS2_LAUNCH_PID=$(docker exec "$CONTAINER_NAME" pgrep -f "ros2 run py_mowbot_utils system_monitor_2")
 
 if [ -z "$ROS2_LAUNCH_PID" ]; then
     echo "No ROS2 launch process found in container $CONTAINER_NAME."
