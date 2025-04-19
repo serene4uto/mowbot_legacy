13a7a711a4f4430c492f315a04f90870267685b3
Zeki411 <nguyenhatrung411@gmail.com>
2025-04-19 19:06:32 +0000

    Add udev rules execution to install script for improved setup process

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ HOME_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 # get the script directory
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# run udev rules
+bash "$SCRIPT_DIR/udev/create_udev_rules.sh"
+
 # ensure ssh-agent is running
 eval "$(ssh-agent -s)"
 # add ssh key to agent
@@ -15,6 +18,7 @@ bash "$SCRIPT_DIR/docker/build.sh"
 
 # copy common data directory to $HOME_DIR
 cp -r "$SCRIPT_DIR/mowbot_legacy_data" "$HOME_DIR/"
+
 
 # remove src
 # rm -rf "$SCRIPT_DIR/src"
